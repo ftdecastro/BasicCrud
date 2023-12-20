18785ff1cc4d23193f1d1bf3422b9380f9f40363
DESKTOP-J2II0SI\ftdec <ftdecastro@gmail.com>
2023-12-20 13:49:41 +0800

    Initial Commit

---
+++ BasicCrud/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicCrud.Controllers
+{
+    public class ProductsList
+    {
+        public static List<Product> MyList = new List<Product>();
+
+        public ProductsList()
+        {
+            Product product = new Product()
+                {
+                    Name = "Coke",
+                    Unit = "Bottle",
+                    Price = 60,
+                    DateOfExpiry = new DateTime(2022, 02, 02),
+                    AvailableInventory = 20
+                };
+            MyList.Add(product);
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProduct")]
+        public Product GetProduct(int index)
+        {
+            return ProductsList.MyList[index];
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Product> GetProducts(int index)
+        {
+            return ProductsList.MyList;
+        }
+
+
+        [HttpPost(Name = "CreateProduct")]
+        public IEnumerable<Product> CreateProduct(String name, String unit, double price, DateTime dateOfExpiry, int availableInventory)
+        {
+            Product newProduct = new Product()
+            {
+                Name = name,
+                Unit = unit,
+                Price = price,
+                DateOfExpiry = dateOfExpiry,
+                AvailableInventory = availableInventory
+            };
+
+            ProductsList.MyList.Add(newProduct);
+
+            return ProductsList.MyList;
+        }
+
+        [HttpPut(Name = "UpdateProduct")]
+        public IEnumerable<Product> UpdateProduct(int index, String name, String unit, double price, DateTime dateOfExpiry, int availableInventory)
+        {
+            ProductsList.MyList[index].Name = name;
+            ProductsList.MyList[index].Unit = unit;
+            ProductsList.MyList[index].Price = price;
+            ProductsList.MyList[index].DateOfExpiry = dateOfExpiry;
+            ProductsList.MyList[index].AvailableInventory = availableInventory;
+
+            return ProductsList.MyList;
+        }
+
+        [HttpDelete(Name = "DeleteProduct")]
+        public IEnumerable<Product> DeleteProduct(int index)
+        {
+            ProductsList.MyList.RemoveAt(index);
+
+            return ProductsList.MyList;
+        }
+
+    }
+}
---
+++ BasicCrud/Product.cs
@@ -0,0 +1,15 @@
+namespace BasicCrud
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public String Unit { get; set; }
+
+        public Double Price { get; set; }
+
+        public DateTime DateOfExpiry { get; set; }
+
+        public int AvailableInventory { get; set; }
+    }
+}
